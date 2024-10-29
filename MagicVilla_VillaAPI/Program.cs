0b5d50d0b416b26032b79801fe86e4cae82e7833
Admin <Admin@DESKTOP-DFAIJ8K>
2024-10-29 12:06:10 +0200

    Add project files.

---
+++ MagicVilla_VillaAPI/Program.cs
@@ -0,0 +1,30 @@
+using MagicVilla_VillaAPI.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers(option => {
+    //option.ReturnHttpNotAcceptable = true;
+    }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ILogging, LoggingV2>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
